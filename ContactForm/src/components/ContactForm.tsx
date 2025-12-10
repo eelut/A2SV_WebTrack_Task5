ecdf662923ba4506a5591b01bd48cac0f4cc2c0e
eelut <endeshawruth@gmail.com>
2025-12-10 14:05:50 +0300

    feat: integrate React Hook Form and DevTools into contact form

--- ContactForm/src/components/ContactForm.tsx
+++ ContactForm/src/components/ContactForm.tsx
@@ -1,7 +1,20 @@
 import "../styles/ContactForm.css";
+import {useForm} from 'react-hook-form';
+import {DevTool} from '@hookform/devtools';
 
+type FormValues={
+  name:string;
+  email:string;
+  message:string;
+}
 
 const ContactForm = () => {
+  const form=useForm<FormValues>();
+  const{register,control,handleSubmit}=form;
+  const onSubmit=(data:FormValues)=>{
+    console.log("Form Submitted",data);
+  };
+
   return (
     <div>
       <section className="contactSection">
@@ -13,31 +26,33 @@ const ContactForm = () => {
             respond as soon as possible
           </p>
           <div className="formCard">
-          <form action="">
+          <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="name">Name</label>
             <input
               type="text"
-              name=""
               id="name"
               placeholder="Enter your name"
+              {...register('name')}
             />
             
             <label htmlFor="email">Email</label>
             <input
               type="email"
-              name=""
               id="email"
               placeholder="Enter your email"
+              {...register('email')}
+
             />
             <label htmlFor="message">Message</label>
             <textarea
-              name="message"
               id="message"
               placeholder="Enter your message"
+              {...register('message')}
               rows={5}
             />
             <button type="submit" className="submitBtn">Submit</button>
           </form>
+          <DevTool control={control}/>
           </div>
         </div>
       </section>
